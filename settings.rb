7fe8f043df20fe9af6e8f83a66c3ba2538f0267d
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2014-02-15 23:12:39 +0100

    ajout de variables & getter

--- settings.rb
+++ settings.rb
@@ -1,9 +1,10 @@
 class Settings
   attr_reader :irc_nickname, :irc_user_comment, :irc_hostname, :irc_channels,
               :channel_warning, :channel_alert, :channel_info, :channel_test,
-              :amqp_exchange_name, :amqp_url
+              :amqp_exchange_name, :amqp_url,
+              :mail_host, :mail_startrls, :mail_domain, :mail_port, :mail_default_recipients, :mail_default_subject, :mail_from
 
-  def initialize
+  def initialize()
     #@amqp_url="amqp://jpierre03:toto@bc.antalios.com:5672"
     @amqp_url="amqp://localhost:5672"
     @amqp_exchange_name= "dev.tmp"
@@ -17,5 +18,14 @@ class Settings
     @irc_channels= [@channel_alert, @channel_info, @channel_test, @channel_warning]
     @irc_nickname="[jpierre03_bot2]"
     @irc_user_comment="Je suis un bot AMQP -> IRC. Je n'aime pas répondre aux gens que je ne connais pas. Je ne connais personne."
+
+    @mail_host="192.168.1.50"
+    @mail_domain="example.com"
+    @mail_port=25
+    @mail_startrls=false
+    @mail_from="sender@example.com"
+    @mail_default_recipients=["test1@spam.prunetwork.fr", "test2@spam.prunetwork.fr"]
+    @mail_default_subject="This is a subject line"
   end
 end
+
