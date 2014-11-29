76f790e7f489ebd42f01bfa858878f96ef7154a2
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2014-11-29 22:23:21 +0100

    move into package

---
+++ common-libs/src/main/java/edu/princeton/cs/algs4/Test.java
@@ -0,0 +1,11 @@
+package edu.princeton.cs.algs4;
+
+public final class Test {
+
+    public static void main(String[] args) {
+        String[] lines = StdIn.readAllLines();
+        for (int i = 0; i < lines.length; i++)
+            StdOut.println(lines[i]);
+    }
+
+}
