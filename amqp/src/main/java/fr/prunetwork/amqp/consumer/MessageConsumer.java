cb22d122c20b4dd9978de108b2df5f5a6fb705f3
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2014-12-11 22:48:49 +0100

    import de classes à migrer

---
+++ amqp/src/main/java/fr/prunetwork/amqp/consumer/MessageConsumer.java
@@ -0,0 +1,17 @@
+package fr.prunetwork.amqp.consumer;
+
+import com.rabbitmq.client.QueueingConsumer;
+import fr.prunetwork.amqp.AmqpReceivedMessage;
+
+/**
+ * @author Jean-Pierre PRUNARET
+ * @since 01/07/2014
+ */
+public interface MessageConsumer {
+
+    void init(QueueingConsumer consumer);
+
+    boolean isConnected();
+
+    AmqpReceivedMessage consume() throws InterruptedException;
+}
