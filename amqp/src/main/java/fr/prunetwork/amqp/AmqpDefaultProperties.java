b0e4d1274b96d0df7072cb4d0b79e7f5bbcd5357
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2014-12-11 22:47:31 +0100

    ajout de valeurs par défauts

--- amqp/src/main/java/fr/prunetwork/amqp/AmqpDefaultProperties.java
+++ amqp/src/main/java/fr/prunetwork/amqp/AmqpDefaultProperties.java
@@ -1,5 +1,8 @@
 package fr.prunetwork.amqp;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Jean-Pierre PRUNARET
  * @since 11/12/14
@@ -9,6 +12,7 @@ public class AmqpDefaultProperties {
     public static final String URI = "amqp://localhost";
     public static final String EXCHANGE = "dev.tmp";
     public static final String ROUTING_KEY = "";
+    public static final List<String> ROUTING_KEYS = Arrays.asList("#");
 
     public AmqpDefaultProperties() {
     }
