bcba0c91b333d6f160c6906a22284af846082796
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2014-12-14 15:45:21 +0100

    ajout de commentaires

--- amqp/src/main/java/fr/prunetwork/amqp/AmqpReceiver.java
+++ amqp/src/main/java/fr/prunetwork/amqp/AmqpReceiver.java
@@ -8,9 +8,22 @@ import java.io.IOException;
  */
 public interface AmqpReceiver<T extends AmqpReceivedMessage> {
 
+    /**
+     * Define settings to use AMQP broker and connect
+     * @throws Exception
+     */
     void configure() throws Exception;
 
+    /**
+     * This method is used to fetch messages one-by-one.
+     * @return a fully constred java object or fail with exception
+     * @throws Exception Exception is thrown if one or more field is missing.
+     */
     T consume() throws Exception;
 
+    /**
+     * Close connexion with AMQP broker
+     * @throws IOException
+     */
     void close() throws IOException;
 }
