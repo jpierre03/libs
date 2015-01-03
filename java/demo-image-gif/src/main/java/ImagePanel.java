1e71d6aa3518d416012c6550e73025237005bdc9
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-01-03 13:01:57 +0100

    indentation

--- java/demo-image-gif/src/main/java/ImagePanel.java
+++ java/demo-image-gif/src/main/java/ImagePanel.java
@@ -8,35 +8,35 @@ import java.awt.*;
  */
 public class ImagePanel extends JPanel {
 
-	private ImageIcon image;
+    private ImageIcon image;
 
-	public ImagePanel(String file) {
-		image = new ImageIcon(file);
-		this.setSize(image.getIconWidth(), image.getIconHeight());
-		this.setMinimumSize(getSize());
-	}
+    public ImagePanel(String file) {
+        image = new ImageIcon(file);
+        this.setSize(image.getIconWidth(), image.getIconHeight());
+        this.setMinimumSize(getSize());
+    }
 
-	public static void main(String args[]) {
-		JFrame frame = new JFrame("Un gif animé");
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Un gif animé");
 
-		ImagePanel panel = new ImagePanel("test.gif");
-		frame.add(panel);
-		frame.setSize(panel.getSize());
+        ImagePanel panel = new ImagePanel("test.gif");
+        frame.add(panel);
+        frame.setSize(panel.getSize());
 
-		//---
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //---
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //		frame.pack();
-		frame.setVisible(true);
-	}
+        frame.setVisible(true);
+    }
 
-	protected void paintComponent(Graphics g) {
-		// efface le contenu précédent :
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		// dessine le gif :
-		// en boucle
-		g.drawImage(image.getImage(), 1, 1, image.getIconWidth(), image.getIconHeight(), this);
-		//une seule fois
-		//g.drawImage(image.getImage(), 1, 1, image.getIconWidth(), image.getIconWidth(), null);
-	}
+    protected void paintComponent(Graphics g) {
+        // efface le contenu précédent :
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        // dessine le gif :
+        // en boucle
+        g.drawImage(image.getImage(), 1, 1, image.getIconWidth(), image.getIconHeight(), this);
+        //une seule fois
+        //g.drawImage(image.getImage(), 1, 1, image.getIconWidth(), image.getIconWidth(), null);
+    }
 }
