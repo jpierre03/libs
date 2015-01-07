9fe33d9919a7b3a6aef262339c47cca0d8507894
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-01-07 14:17:52 +0100

    mini scénario de test

--- java/json/src/main/java/fr/prunetwork/json/JsonPrinterTest.java
+++ java/json/src/main/java/fr/prunetwork/json/JsonPrinterTest.java
@@ -1,11 +1,21 @@
 package fr.prunetwork.json;
 
+import org.json.JSONObject;
+
 /**
  * @author Jean-Pierre PRUNARET
  * @since 07/01/15
  */
 public class JsonPrinterTest {
 
-    public JsonPrinterTest() {
+    private JsonPrinterTest() {
+    }
+
+    public static void main(String... args) {
+        String json = "{\"type\":\"home\",\"number\":\"(800) 111-1111\"}";
+
+        final JSONObject object = new JSONObject(json);
+
+        JsonPrinter.printRecursive(object);
     }
 }
