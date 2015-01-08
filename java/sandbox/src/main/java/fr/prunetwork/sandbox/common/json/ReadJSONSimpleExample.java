93d1aa92e7c28beffd15613bb85bbd15cdea5c7a
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-01-08 09:40:56 +0100

    inutile

--- java/sandbox/src/main/java/fr/prunetwork/sandbox/common/json/ReadJSONSimpleExample.java
+++ java/sandbox/src/main/java/fr/prunetwork/sandbox/common/json/ReadJSONSimpleExample.java
@@ -18,67 +18,6 @@ public class ReadJSONSimpleExample {
 
     public static void main(String[] args) {
 
-        JSONParser parser = new JSONParser();
-
-        try {
-            Object obj = parser.parse(new FileReader("test.json"));
-
-            JSONObject jsonObject = (JSONObject) obj;
-
-            MyStaticClass.printRecursive(jsonObject);
-
-            /*
-            //display(jsonObject);
-
-            final Map<SimpleJsonField, Object> map = new TreeMap<>();
-            jsonObject.forEach(new BiConsumer<String, Object>() {
-                @Override
-                public void accept(String o, Object o2) {
-                    map.put(SimpleJsonField.valueOf(o), o2);
-                }
-
-                @Override
-                public BiConsumer andThen(BiConsumer after) {
-                    return null;
-                }
-            });
-
-
-
-            final StringBuilder sb = new StringBuilder();
-
-            for (Map.Entry<SimpleJsonField, Object> entry : map.entrySet()) {
-                sb.append("key: ").append(entry.getKey());
-                sb.append(" v: ").append(entry.getValue());
-                sb.append("\n");
-            }
-            System.out.println(sb.toString());
-
-
-            String name = (String) jsonObject.get(SimpleJsonField.name);
-            System.out.println(name);
-
-            Integer age = (Integer) jsonObject.get(SimpleJsonField.age);
-            System.out.println(age);
-
-            // loop array
-            final Object messages = jsonObject.get(SimpleJsonField.messages);
-            JSONArray fields = (JSONArray) messages;
-            for (Object field : fields) {
-                System.out.println(field.toString());
-            }
-*/
-
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("IOException");
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     private static void display(JSONObject jsonObject) {
