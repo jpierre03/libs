c3a8a49347dcf5289f58bc08e1d9e2d19746e6ab
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-01-08 10:41:37 +0100

    comments

--- java/sandbox/src/main/java/fr/prunetwork/snipet/ComponentEventDemo.java
+++ java/sandbox/src/main/java/fr/prunetwork/snipet/ComponentEventDemo.java
@@ -45,6 +45,7 @@ import java.awt.event.ItemListener;
 public class ComponentEventDemo extends JPanel
         implements ComponentListener,
         ItemListener {
+
     static JFrame frame;
     JTextArea display;
     JLabel label;
@@ -74,12 +75,40 @@ public class ComponentEventDemo extends JPanel
         frame.addComponentListener(this);
     }
 
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        /** Create and set up the window. */
+        frame = new JFrame("ComponentEventDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /** Create and set up the content pane. */
+        JComponent newContentPane = new ComponentEventDemo();
+        newContentPane.setOpaque(true); //content panes must be opaque */
+        frame.setContentPane(newContentPane);
+
+        /** Display the window. */
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        /** Schedule a job for the event-dispatching thread: creating and showing this application's GUI. */
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
     public void itemStateChanged(ItemEvent e) {
         if (e.getStateChange() == ItemEvent.SELECTED) {
             label.setVisible(true);
 
-            //Need to revalidate and repaint, or else the label
-            //will probably be drawn in the wrong place.
+            /** Need to validate and repaint, or else the label will probably be drawn in the wrong place. */
             label.revalidate();
             label.repaint();
         } else {
@@ -88,11 +117,8 @@ public class ComponentEventDemo extends JPanel
     }
 
     protected void displayMessage(String message) {
-        //If the text area is not yet realized, and
-        //we tell it to draw text, it could cause
-        //a text/AWT tree deadlock. Our solution is
-        //to ensure that the text area is realized
-        //before attempting to draw text.
+        /** If the text area is not yet realized, and we tell it to draw text, it could cause a text/AWT tree deadlock. Our solution is
+         to ensure that the text area is realized before attempting to draw text. */
         // if (display.isShowing()) {
         display.append(message + newline);
         display.setCaretPosition(display.getDocument().getLength());
@@ -114,35 +140,5 @@ public class ComponentEventDemo extends JPanel
     public void componentShown(ComponentEvent e) {
         displayMessage(e.getComponent().getClass().getName() + " --- Shown");
 
-    }
-
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        frame = new JFrame("ComponentEventDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Create and set up the content pane.
-        JComponent newContentPane = new ComponentEventDemo();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
     }
 }
