2b4599268a6bbe8e65b69a64bce894ee9233a325
Jean-Pierre PRUNARET <jp.prunaret@antalios.com>
2015-01-14 17:06:41 +0100

    utilisation de la configuration du module et non locale

--- java/amqp/src/test/java/fr/prunetwork/amqp/ReceiveLogsTopic.java
+++ java/amqp/src/test/java/fr/prunetwork/amqp/ReceiveLogsTopic.java
@@ -8,24 +8,24 @@ import com.rabbitmq.client.QueueingConsumer;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static fr.prunetwork.amqp.AmqpDefaultProperties.EXCHANGE;
+import static fr.prunetwork.amqp.AmqpDefaultProperties.URI;
+
 /**
  * @author Jean-Pierre PRUNARET
  * @since 14/05/2014
  */
 public class ReceiveLogsTopic {
 
-    static final String AMQP_TEST_URL = "amqp://localhost";
-    static final String AMQP_TEST_EXCHANGE = "test";
-
     public static void main(String[] argv)
             throws Exception {
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setUri(AMQP_TEST_URL);
+        factory.setUri(URI);
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.exchangeDeclare(AMQP_TEST_EXCHANGE, "topic");
+        channel.exchangeDeclare(EXCHANGE, ExchangeType.topic.toString());
         String queueName = channel.queueDeclare().getQueue();
 
         ArrayList<String> bindingKeys = new ArrayList<>();
@@ -41,7 +41,7 @@ public class ReceiveLogsTopic {
         bindingKeys.add("#");
 
         for (String bindingKey : bindingKeys) {
-            channel.queueBind(queueName, AMQP_TEST_EXCHANGE, bindingKey);
+            channel.queueBind(queueName, EXCHANGE, bindingKey);
         }
 
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
