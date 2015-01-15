1f59a247ce97c0fc6271e849aa649d756b2bfe9f
Jean-Pierre PRUNARET <jp.prunaret@antalios.com>
2015-01-15 11:25:01 +0100

    ajustement nommage pour satisfaire la génération de la javadoc

--- java/elasticsearch-json-poster/src/main/java/fr/prunetwork/status/Anomalie.java
+++ java/elasticsearch-json-poster/src/main/java/fr/prunetwork/status/Anomalie.java
@@ -8,15 +8,15 @@ public enum Anomalie {
     travail_hors_zone,
     travail_trop_rapide,
     travail_trop_lent,
-    zone_traitée_en_double,
+    zone_traitee_en_double,
     travail_dans_la_mauvaise_zone,
     absence_du_personnel,
-    alerte_déclenchée_par_le_personnel,
-    machine_sortie_de_zone_autorisée,
+    alerte_declenchee_par_le_personnel,
+    machine_sortie_de_zone_autorisee,
     machine_immobile,
-    machine_qui_se_déplace_sans_brosser,
-    machine_durée_utilisation_supérieure_au_contrat,
-    machine_géolocalisation_en_panne,
+    machine_qui_se_deplace_sans_brosser,
+    machine_duree_utilisation_superieure_au_contrat,
+    machine_geolocalisation_en_panne,
     machine_muette,
-    capteur_défaillant
+    capteur_defaillant
 }
