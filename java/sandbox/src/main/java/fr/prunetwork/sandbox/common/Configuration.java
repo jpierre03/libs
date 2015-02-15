6c6c011774192eaaa2a9dd995bf7d57b08581133
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-02-15 20:42:00 +0100

    ajustement de nom de package

--- java/sandbox/src/main/java/fr/prunetwork/sandbox/common/Configuration.java
+++ java/sandbox/src/main/java/fr/prunetwork/sandbox/common/Configuration.java
@@ -1,4 +1,4 @@
-package fr.onet.ae.common;
+package fr.prunetwork.sandbox.common;
 
 /**
  * @author Jean-Pierre PRUNARET
@@ -6,8 +6,7 @@ package fr.onet.ae.common;
  */
 public final class Configuration {
 
-    //public static final String AMQP_DEFAULT_URL ="amqp://jpierre03:toto@localhost";
-    public static final String AMQP_DEFAULT_URL ="amqp://jpierre03:toto@172.16.201.201";
+    public static final String AMQP_DEFAULT_URL ="amqp://jpierre03:toto@localhost";
     public static final String AMQP_DEFAULT_EXCHANGE="dev.tmp";
     public static final String AMQP_DEFAULT_ROUTING_KEY = "";
 
