23c45162971c077c0f60def1a0b672f86b1e7ece
Jean-Pierre PRUNARET <jp.prunaret@antalios.com>
2015-03-19 15:24:41 +0100

    création d'un classe de service

--- java/amqp-mail/src/main/java/fr/prunetwork/mail/AmqpToMailForwarder.java
+++ java/amqp-mail/src/main/java/fr/prunetwork/mail/AmqpToMailForwarder.java
@@ -1,7 +1,65 @@
 package fr.prunetwork.mail;
 
+import fr.prunetwork.amqp.AmqpConfiguration;
+import fr.prunetwork.amqp.message.JsonMessage;
+import fr.prunetwork.amqp.receiver.JsonAmqpReceiver;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
- * Created by jpierre03 on 19/03/2015.
+ * @author Jean-Pierre PRUNARET
+ * @since 2015-03-19
  */
 public class AmqpToMailForwarder {
+
+    @NotNull
+    private final JsonAmqpReceiver receiver;
+    @NotNull
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public AmqpToMailForwarder(@NotNull final AmqpConfiguration amqpConfiguration) throws Exception {
+
+        receiver = new JsonAmqpReceiver(amqpConfiguration);
+        receiver.configure();
+
+
+        executor.execute(() -> {
+            while (true) {
+                try {
+                    @NotNull final JsonMessage message = receiver.consume();
+                    @NotNull final JSONObject json = message.getJson();
+
+                    @NotNull final String subject = json.getString("subject");
+                    @NotNull final String body = json.getString("body");
+                    @NotNull final List<String> destination = new ArrayList<>();
+
+                    @NotNull final JSONArray destinations = json.getJSONArray("destination");
+                    for (int i = 0; i < destinations.length(); i++) {
+                        final String d = destinations.getString(i);
+                        destination.add(d);
+                    }
+
+                    @NotNull final Mail mail = new SimpleMail("toto@example.com", destination, subject, body);
+
+                    message.displayMessage();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+
+                }
+            }
+        });
+    }
+
 }
